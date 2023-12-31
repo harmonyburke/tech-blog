dc26694301c184456f46da09178db06c0f351d26
Harmony Burke <theharmonyburke@gmail.com>
2023-12-31 14:49:43 -0600

    working on routes

--- models/Comment.js
+++ models/Comment.js
@@ -34,6 +34,11 @@ Comment.init(
                 key:'id'
             }
         },
+        date_created:{
+            type: DataTypes.DATE,
+            allowNull:false,
+            defaultValue:DataTypes.NOW
+        },
     },
     {
         sequelize,
