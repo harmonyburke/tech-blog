045e2978c762b811660e2475a912359b74ad0165
Harmony Burke <theharmonyburke@gmail.com>
2024-01-17 19:20:23 -0600

    updated comment routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -15,17 +15,32 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+
+router.post(':id', withAuth, async (req,res) =>{
+  try {
+    const newComment= await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+      post_id:req.params.post_id
+    })
+
+    res.status(200).json(newComment)
+  } catch (err){
+    res.status(400).json(err)
+  }
+});
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const commentData = await Comment.destroy({
       where: {
         id: req.params.id,
-        commentText: req.session.commentText,
+        post_id:req.params.post_id
       },
     });
 
     if (!commentData) {
-      res.status(404).json({ message: 'No comment found with this id!' });
+      res.status(404).json({ message: 'No blo post found with this id!' });
       return;
     }
 
